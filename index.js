2c3e01dab56a7a8e966dcb211b59d9dd0be83059
Matt McKegg <matt@wetsand.co.nz>
2014-10-09 02:13:22 +1300

    allow specifying `triggerSlots` instead of explicit triggers and slots with ids

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function Chunk(opts){
     
     shape: Observ(),
     stride: Observ(),
-    triggers: Observ([]),
+    triggers: Observ(),
+
+    // alternative to specifying `slots` and `triggers` (auto assigns ID)
+    // overrides `triggers`
+    triggerSlots: Observ(),
 
     inputs: Observ([]),
     outputs: Observ([]),
@@ -32,45 +36,69 @@ function Chunk(opts){
   var releases = []
 
   var getGlobalId = opts.getGlobalId || getGlobalIdFallback
-  var resolvedIds = computed([obs.id, obs.triggers], function(id, triggers){
-    if (!Array.isArray(triggers)) triggers = []
-    return triggers.map(lookupGlobal)
+  var resolvedIds = computed([obs.id, obs.triggers, obs.triggerSlots], function(id, triggers, triggerSlots){
+    if (Array.isArray(triggerSlots) && triggerSlots.length){
+      console.log('SLOTSZ')
+      var result = []
+      for (var i=0;i<triggerSlots.length;i++){
+        result.push(lookupGlobal(String(i)))
+      }
+      return result
+    } else {
+      console.log('SLOTSES')
+      if (!Array.isArray(triggers)) triggers = []
+      return triggers.map(lookupGlobal)
+    }
   })
 
-  var resolvedSlots = computedNextTick([obs.id, obs.slots, obs.routes], function(id, slots, routes){
+  var resolvedSlots = computedNextTick([obs.id, obs.slots, obs.triggerSlots, obs.routes], function(id, slots, triggerSlots, routes){
     if (!routes) routes = {}
-    if (!Array.isArray(slots)) slots = []
-
     var result = []
-    for (var i=0;i<slots.length;i++){
-      var slot = slots[i]
-      result[i] = obtainWithIds(slot, lookupGlobal)
-      if (routes[slot.id]){
-        result[i].output = routes[slot.id]
+
+    if (Array.isArray(slots)){
+      for (var i=0;i<slots.length;i++){
+        var slot = slots[i]
+        result[i] = obtainWithIds(slot, lookupGlobal)
+        if (routes[slot.id]){
+          result[i].output = routes[slot.id]
+        }
+        result[i].output
       }
-      result[i].output
     }
+
+    if (Array.isArray(triggerSlots)){
+      for (var i=0;i<triggerSlots.length;i++){
+        var id = String(i)
+        var slot = obtainWithIds(triggerSlots[i], lookupGlobal) 
+        slot.id = lookupGlobal(id)
+        if (routes[id]){
+          result[i].output = routes[id]
+        }
+        result.push(slot)
+      }
+    }
+
     return result
   })
 
   var usedSlots = {}
   releases.push(resolvedSlots(function(slots){
-    if (obs.id()){
+    if (obs.id() && slots){
       slots.forEach(function(descriptor){
         // naive and slow implementation, but soundbank performs deepEqual checks on update
         // hoepfully this will make things OK :/
         usedSlots[descriptor.id] = true
         opts.soundbank.update(descriptor)
       })
+    }
 
-      // clean up unused slots
-      for (var id in usedSlots){
-        if (usedSlots[id] === false){
-          ;delete usedSlots[id]
-          opts.soundbank.remove(id)
-        } else {
-          usedSlots[id] = false
-        }
+    // clean up unused slots
+    for (var id in usedSlots){
+      if (usedSlots[id] === false){
+        ;delete usedSlots[id]
+        opts.soundbank.remove(id)
+      } else {
+        usedSlots[id] = false
       }
     }
   }))
